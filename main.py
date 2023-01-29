b7498b7442a2fcee6225b57fe9f64a9cf3a56cdb
macbookpro <dansmailik00@gmail.com>
2023-01-29 10:23:43 -0500

    hello

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-class Student:
 
-    group = "B29234"
-    education = "Itstep"
-    teacher = "Evgeniy"
+class Drink:
 
+    Gaz_drink = "Coca_cola"
+    Juice = "Orange_Juice"
+    Milk = "Chocolate_milk"
+    Kompot = "Strawberry_kompot"
     def __init__(self):
         print(id(self))
 
-st1 = Student()
-print(id(st1))
+dr2 = Drink()
 
-print(Student.group)
+print(Drink.Gaz_drink)
+print(Drink.Juice)
+print(Drink.Milk)
+print(Drink.Kompot)
+
+class Food:
+
+    Snacks = "Meat_Balls"
+    pourige = "Flakes"
+    Meat = "Beef_steak"
+    Fish = "Skumbriya"
+    def __init__(self):
+        print(id(self))
+
+fd1 = Food()
+
+print(Food.Snacks)
+print(Food.pourige)
+print(Food.Meat)
+print(Food.Fish)
+
+class Pearsonal:
+
+    Waiter = "Nikita"
+    Waitress = "Elena"
+    Chef = "Igor"
+    Boss = "Konstantin"
+    def __init__(self):
+        print(id(self))
+
+Pr3 = Pearsonal()
+
+print(Pearsonal.Chef)
+print(Pearsonal.Waiter)
+print(Pearsonal.Waitress)
+print(Pearsonal.Boss)
